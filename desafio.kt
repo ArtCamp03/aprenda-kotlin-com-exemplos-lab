a28646b53fedca6a19af2c8f935fb83b7c3896f7
Artur R Campos <62857760+ArtCamp03@users.noreply.github.com>
2023-04-24 11:23:48 -0300

    attt da atividade

--- desafio.kt
+++ desafio.kt
@@ -1,7 +1,3 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
-enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
-
 class Usuario
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
@@ -11,11 +7,11 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario.toString())
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudo = ConteudoEducacional("Fisica", 60)
+    Formacao("Fisica",onteudo)
 }
